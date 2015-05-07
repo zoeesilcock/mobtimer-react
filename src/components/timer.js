e67cc48d199d4b7b255ae4bd0786e38247ad67c6
Zoee Silcock <mrzoee@gmail.com>
2015-05-07 12:43:15 +0200

    Use moment.js to calculate and format time left

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Moment from 'moment';
 
 class Timer extends React.Component {
+  formatTime() {
+    return Moment(this.props.msLeft).format('mm:ss');
+  }
+
   render() {
     return (
-      <div>{this.props.msLeft}</div>
+      <div>{this.formatTime()}</div>
     );
   }
 };
--- src/stores/timer_store.js
+++ src/stores/timer_store.js
@@ -1,4 +1,5 @@
 import Reflux from 'reflux';
+import Moment from 'moment';
 import Actions from '../actions/timer_actions';
 
 var Store = Reflux.createStore({
@@ -8,6 +9,7 @@ var Store = Reflux.createStore({
     this.data = {
       minutes: 30,
       msLeft: 0,
+      end: 0,
       state: 'idle' // idle -> running -> paused
     };
   },
@@ -17,8 +19,9 @@ var Store = Reflux.createStore({
   },
 
   onStart() {
-    this.data.msLeft = this.data.minutes * 1000;
     this.data.state = 'running';
+    this.data.end = Moment().add(this.data.minutes, 'minutes');
+    this.updateTimeLeft();
     this.trigger();
   },
 
@@ -30,6 +33,18 @@ var Store = Reflux.createStore({
   onReset() {
     this.data.state = 'idle';
     this.trigger();
+  },
+
+  scheduleUpdate() {
+    window.setTimeout(this.updateTimeLeft, 1000);
+  },
+
+  updateTimeLeft() {
+    if (this.data.state == 'running') {
+      this.data.msLeft = this.data.end.diff(Moment(), 'milliseconds');
+      this.trigger();
+      this.scheduleUpdate();
+    }
   }
 });
 
